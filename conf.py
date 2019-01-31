ba35e54db415ee92b6abf76eefe8bdb96faaa6db
Morgan <morganisverycute@gmail.com>
2019-01-30 18:38:28 -0600

    added framework for shortening, config

---
+++ conf.py
@@ -0,0 +1,8 @@
+# 404 Redirect URL
+home_url = 'https://reasons-to.live/'
+# URL Sketchify options
+placeholder = True
+# URL Shorten options
+rand_size = 6
+seed_index = False #adds 2 to rand_size
+chars = string.ascii_letters + string.digits
--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, redirect
+import random, conf
+#TO-DO: load links from save medium
+links = {}
+
+#mode 0 = shorten, mode 1 = sketchify
+def generate(mode):
+    if mode:
+        #TO-DO: make sketchy string
+    else:
+        while True:
+            output = ''.join(random.choice(conf.chars) for _ in range(conf.rand_size))
+            if conf.seed_index:
+                random.seed(len(links))
+                ind = str(random.random() * 100)
+                output = ''.join([ind[0], output, ind[1]])
+            #guarentees each string is unique
+            if not output in links: break
+        return output
+
 app = Flask(__name__)
 application = app # required for passenger_wsgi
+@app.route('/geturl&target=<target>&mode=<mode>')
+def geturl():
+    if target is None:
+        return 'invalid target!'
+    try: mode = int(mode)
+    except: return 'invalid mode!'
+    link = generate(mode)
+    links[link] = target
+    #TO-DO: save links either to SQL or JSON or some other thing
+    return link
 
-@app.route('/fuck')
-def test():
-    return 'asdf'
+#to be ran on 5hl.pw
+@app.route('/<destination>')
+def redir(destination):
+    if destination in links: return redirect(links[destination])
+    return redirect(conf.home_url)
 
 # for testing
 if __name__ == '__main__': app.run()
