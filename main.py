fd96668b6de007a9e91d793998057136aad845d7
Morgan <morganisverycute@gmail.com>
2019-01-30 18:47:52 -0600

    added persistence to links

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect
-import random, conf
-#TO-DO: load links from save medium
-links = {}
+from flask import Flask, redirect, abort
+import random, conf, json
+
+#loading redirect links
+with open('links.json', 'r', encoding='utf-8') as db:
+    links = json.load(db)
 
 #mode 0 = shorten, mode 1 = sketchify
 def generate(mode):
@@ -21,15 +23,23 @@ def generate(mode):
 app = Flask(__name__)
 application = app # required for passenger_wsgi
 @app.route('/geturl&target=<target>&mode=<mode>')
-def geturl():
+def geturl(target, mode):
     if target is None:
         return 'invalid target!'
     try: mode = int(mode)
     except: return 'invalid mode!'
+        
     link = generate(mode)
     links[link] = target
-    #TO-DO: save links either to SQL or JSON or some other thing
+    with open('links.json', 'w', encoding='utf-8') as db:
+        json.dump(links,db,ensure_ascii=False,indent=4)
+
     return link
+
+#ghetto security
+@app.route('/links.json')
+def fuckyou():
+    return 'go commit the big die', 403
 
 #to be ran on 5hl.pw
 @app.route('/<destination>')
