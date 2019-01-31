fef0aa3779d961c96976afdf6e9a5ee0b1e650bb
Morgan <morganisverycute@gmail.com>
2019-01-30 19:51:53 -0600

    added duplicate redir check to mode 0

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ with open('links.json', 'r', encoding='utf-8') as db:
     links = json.load(db)
 
 #mode 0 = shorten, mode 1 = sketchify
-def generate(mode):
+def generate(target, mode):
     if mode:
         #TO-DO: make sketchy string
         pass
     else:
+        #mode 0 doesn't support generation of multiple URL's pointing to the same target
+        for link, val in links.items():
+            if target == val: return (link, False)
+
         while True:
             output = ''.join(random.choice(conf.chars) for _ in range(conf.rand_size))
             if conf.seed_index:
@@ -19,7 +23,7 @@ def generate(mode):
                 output = ''.join([ind[0], output, ind[1]])
             #guarentees each string is unique
             if not output in links: break
-        return output
+        return (output, True)
 
 app = Flask(__name__)
 application = app # required for passenger_wsgi
@@ -27,11 +31,14 @@ application = app # required for passenger_wsgi
 def geturl():
     target = request.args.get('target', default = 'https://reasons-to.live/', type = str)
     mode = request.args.get('mode', default = 0, type = int)
-    link = generate(mode)
-    links[link] = target
-    with open('links.json', 'w', encoding='utf-8') as db:
-        json.dump(links,db,ensure_ascii=False,indent=4)
-        
+    data = generate(target, mode)
+    link = data[0]
+    #only saves if a NEW link has been generated
+    if data[1]:
+        links[link] = target
+        with open('links.json', 'w', encoding='utf-8') as db:
+            json.dump(links,db,ensure_ascii=False,indent=4)
+
     return link
 
 #ghetto 'security'
